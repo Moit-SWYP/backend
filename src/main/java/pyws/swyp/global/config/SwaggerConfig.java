9f6831c665187a4a03eae4649f05f569b15f3183
Jo Myeong-geun <mg6359@naver.com>
2025-12-04 16:55:42 +0900

    feat: 스웨거 설정 추가

--- src/main/java/pyws/swyp/global/config/SwaggerConfig.java
+++ src/main/java/pyws/swyp/global/config/SwaggerConfig.java
@@ -1,7 +1,44 @@
 package pyws.swyp.global.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("SWYP API")
+                        .description("SWYP API 명세서입니다.")
+                        .version("v1.0"));
+    }
+
+    @Bean
+    public GroupedOpenApi memberApi() {
+        return GroupedOpenApi.builder()
+                .group("member")
+                .pathsToMatch("/api/members/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi meetingApi() {
+        return GroupedOpenApi.builder()
+                .group("meeting")
+                .pathsToMatch("/api/meetings/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi authApi() {
+        return GroupedOpenApi.builder()
+                .group("auth")
+                .pathsToMatch("/api/auth/**")
+                .build();
+    }
 }
