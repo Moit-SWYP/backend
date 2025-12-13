4ffa9155e232056efea59e26e6cb8ebefe31252f
Jo Myeong-geun <mg6359@naver.com>
2025-12-14 05:05:10 +0900

    chore: #9 불필요한 애노테이션 제거

--- src/main/java/pyws/swyp/global/config/RedisConfig.java
+++ src/main/java/pyws/swyp/global/config/RedisConfig.java
@@ -1,13 +1,11 @@
 package pyws.swyp.global.config;
 
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
-@EnableConfigurationProperties
 public class RedisConfig {
 
     @Bean
