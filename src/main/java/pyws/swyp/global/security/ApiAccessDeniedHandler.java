423bda04cb8330f16505177fbbe665aae86d84a5
Jo Myeong-geun <mg6359@naver.com>
2025-12-14 05:07:22 +0900

    feat: #9 ObjectMapper 싱글톤 빈 등록

--- src/main/java/pyws/swyp/global/security/ApiAccessDeniedHandler.java
+++ src/main/java/pyws/swyp/global/security/ApiAccessDeniedHandler.java
@@ -1,17 +1,21 @@
 package pyws.swyp.global.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 import pyws.swyp.global.error.ErrorCode;
 import pyws.swyp.global.error.ErrorResponse;
-import tools.jackson.databind.ObjectMapper;
 
 @Component
+@RequiredArgsConstructor
 public class ApiAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper objectMapper;
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response,
@@ -22,6 +26,6 @@ public class ApiAccessDeniedHandler implements AccessDeniedHandler {
 
         ErrorResponse body = new ErrorResponse(ErrorCode.FORBIDDEN);
 
-        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.getWriter().write(objectMapper.writeValueAsString(body));
     }
 }
--- src/main/java/pyws/swyp/global/security/ApiAuthenticationEntryPoint.java
+++ src/main/java/pyws/swyp/global/security/ApiAuthenticationEntryPoint.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -11,7 +12,10 @@ import pyws.swyp.global.error.ErrorCode;
 import pyws.swyp.global.error.ErrorResponse;
 
 @Component
+@RequiredArgsConstructor
 public class ApiAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
@@ -22,6 +26,6 @@ public class ApiAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
         ErrorResponse body = new ErrorResponse(ErrorCode.UNAUTHORIZED);
 
-        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.getWriter().write(objectMapper.writeValueAsString(body));
     }
 }
