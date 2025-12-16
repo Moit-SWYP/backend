3c8c20f9d70b2d03786bcd45b1ce2aa61661ade0
Jo Myeong-geun <mg6359@naver.com>
2025-12-17 06:39:37 +0900

    feat: BaseEntity에 논리 삭제 필터 자동 적용

--- src/main/java/pyws/swyp/global/entity/BaseEntity.java
+++ src/main/java/pyws/swyp/global/entity/BaseEntity.java
@@ -1,14 +1,23 @@
 package pyws.swyp.global.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
 import lombok.Getter;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.FilterDef;
 import org.hibernate.annotations.UpdateTimestamp;
-
-import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Getter
+@FilterDef(
+        name = "activeFilter",
+        defaultCondition = "is_active = true",
+        autoEnabled = true
+)
 public class BaseEntity {
 
     @Id
