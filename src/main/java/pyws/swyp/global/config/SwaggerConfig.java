ff863f33a5351a6870272093efe492d6eda8679c
Jo Myeong-geun <mg6359@naver.com>
2025-12-29 22:50:15 +0900

    feat: Swagger 서버 URL https로 설정

--- src/main/java/pyws/swyp/global/config/SwaggerConfig.java
+++ src/main/java/pyws/swyp/global/config/SwaggerConfig.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,7 +16,10 @@ import org.springframework.context.annotation.Configuration;
                 title = "Moit-SWYP API",
                 description = "Moit-SWYP API 명세서입니다.",
                 version = "v1.0"
-        )
+        ),
+        servers = {
+                @Server(url = "https://moit.shop/")
+        }
 )
 @SecurityScheme(
         name = "auth",
